45569423ae6c256070d86f248b250c18d30f01e1
JeongHoL <alwjw@naver.com>
2025-03-31 21:46:26 +0900

    Draw 81 rectangles using Index & Vertex Buffer Succeed

--- Client/250331(2) - IndexBuffer/Engine/Mesh.h
+++ Client/250331(2) - IndexBuffer/Engine/Mesh.h
@@ -8,16 +8,27 @@ public:
 
 	void MakeTriangle();
 	void MakeRectangle();
+	void SetRectangleIdx();
 
 	void SetTransform(const Transform& t) { _transform = t; }
+
+private:
+	void CreateVertexBuffer();
+	void CreateIndexBuffer();
 
 private:
 	ComPtr<ID3D12Resource>		_vertexBuffer;
 	D3D12_VERTEX_BUFFER_VIEW	_vertexBufferView = {};
 	uint32						_vertexCount = { 0 };
 
+	ComPtr<ID3D12Resource>		_indexBuffer;
+	D3D12_INDEX_BUFFER_VIEW		_indexBufferView = {};
+	uint32						_indexCount = { 0 };
+
 	vector<Vertex> triangle;
+	
 	vector<Vertex> rectangle;
+	vector<uint32> rectIdx;
 
 	Transform _transform = {};
 };
