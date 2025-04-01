2f2c40487be212d1d22f76245aa7535f24148378
JeongHoL <alwjw@naver.com>
2025-04-01 18:07:12 +0900

    Move Client in differend Branch

---
+++ Client/250331(3) - Texture/Engine/Engine.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "Device.h"
+#include "CommandQueue.h"
+#include "SwapChain.h"
+#include "RootSignature.h"
+#include "Texture.h"
+#include "Mesh.h"
+#include "Shader.h"
+#include "ConstantBuffer.h"
+#include "TableDescriptorHeap.h"
+
+class Engine
+{
+public:
+	void Init(const WindowInfo& info);
+	void Render();
+
+	void RenderBegin();
+	void RenderEnd();
+
+	void ResizeWindow(int32 width, int32 height);
+
+	shared_ptr<Device>				GetDevice() { return _device; }
+	shared_ptr<CommandQueue>		GetCmdQueue() { return _cmdQueue; }
+	shared_ptr<SwapChain>			GetSwapChain() { return	_swapChain; }
+	shared_ptr<RootSignature>		GetRootSignature() { return _rootSignature; }
+	shared_ptr<ConstantBuffer>		GetConstantBuffer() { return _constantBuffer; }
+	shared_ptr<TableDescriptorHeap> GetTableDescHeap() { return _tableDescHeap; }
+
+private:
+	WindowInfo		_window;
+	D3D12_VIEWPORT	_viewPort = {};
+	D3D12_RECT		_scissorRect = {};
+
+	shared_ptr<Device>				_device;
+	shared_ptr<CommandQueue>		_cmdQueue;
+	shared_ptr<SwapChain>			_swapChain;
+	shared_ptr<RootSignature>		_rootSignature;
+	shared_ptr<ConstantBuffer>		_constantBuffer;
+	shared_ptr<TableDescriptorHeap>	_tableDescHeap;
+
+	// 아래 세개는 내가 실험하기 위해 추가한 것
+	shared_ptr<Mesh>				_mesh;
+	shared_ptr<Shader>				_shader;
+	shared_ptr<Texture>				_texture;
+};
