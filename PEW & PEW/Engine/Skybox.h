48c15209bbddc0c55b86f9968666c74617032202
DESKTOP-8R4KCSO\alwjw <alwjw@naver.com>
2025-07-24 19:38:21 +0900

    250724 PEW&PEW Update ( PVP skybox added )

--- PEW & PEW/Engine/Skybox.h
+++ PEW & PEW/Engine/Skybox.h
@@ -12,12 +12,15 @@ public:
 
 	void Draw(const glm::mat4& view, const glm::mat4& projection);
 
+	void ChangeCubeMapTexture() { cubemapTexture = LoadCubemap(faces2); }
+
 private:
 	GLuint VAO, VBO, shaderprogram;
 	GLuint ViewLoc, ProjLoc;
 
 private:
 	vector<string> faces;
+	vector<string> faces2;
 	float skyboxVertices[108];
 	unsigned int cubemapTexture;
 };
