535a76873908715b02325d6dca5ed2ae79fecec9
DESKTOP-8R4KCSO\alwjw <alwjw@naver.com>
2025-09-06 17:53:58 +0900

    250906 Final Update ( Test )

--- 졸작/Final/GameScene.cpp
+++ 졸작/Final/GameScene.cpp
@@ -98,8 +98,8 @@ void GameScene::InitializeLogic()
 		auto transform = flameEffect->AddComponent<Transform>();
 
 		effectRenderer->Initialize(*coreRef);
-		effectRenderer->LoadEffect(u"../Effects/BloodLance.efk");
-		transform->SetPosition(1.f, 0.f, 0.5f);
+		effectRenderer->LoadEffect(u"../Effects/Atmosphere.efk");
+		transform->SetPosition(1.f, 10.f, -10.5f);
 
 		AddGameObject(flameEffect);
 	}
