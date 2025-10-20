a6d4a5d4b8e838f1429ac8c1bf6e3333b7dd095c
DESKTOP-8R4KCSO\alwjw <alwjw@naver.com>
2025-10-20 20:16:13 +0900

    251020 Final Update (추후 기술 추가 시 프로젝트 내 코드 작성 기준 - SSAO.h 주석 참조)

--- 졸작/Final/SSAO.h
+++ 졸작/Final/SSAO.h
@@ -1,4 +1,17 @@
 #pragma once
+
+/*
+1. DX12 core 클래스가 SSAO ptr 들고있기
+->DX12 클래스 몸집 증가
+->외부에서 ssao만 따로 참조할 경우가 생기는가 ? X
+
+2. SSAO 클래스 따로 만들기
+->RootSig나 Shader는 코드의 변동성이 존재하는데 SSAO는 그렇지 않을듯 함
+->굳이 따로 빼야하나 ? 변동성 없는 코드들은 DX12Core에 묶어서 코딩햇는데 ?
+
+=> 클래스 따로 빼는걸로, 나중에 Blur & Bloom 등도 마찬가지로 진행
+*/
+
 class SSAO
 {
 public:
