f0fe2969a0f4e03f0ab5168e2b54645d84a5a65a
Vallhup <ths1220ths@naver.com>
2026-01-21 04:32:07 +0900

    feat: Implement AABB Collision
    OBB Vs Capsule Collision is function-defined only for the implementation of processing flows and does not actually work.

--- WITH_Server/WITH_Server/Collision.h
+++ WITH_Server/WITH_Server/Collision.h
@@ -121,4 +121,18 @@ namespace Collision {
 
 		return distSq <= R * R;
 	}
+
+	bool CheckAABBVsAABB(const AABB& a, const AABB& b)
+	{
+		if (a.max.x < b.min.x || a.min.x > b.max.x) return false;
+		if (a.max.y < b.min.y || a.min.y > b.max.y) return false;
+		if (a.max.z < b.min.z || a.min.z > b.max.z) return false;
+		return true;
+	}
+
+	bool CheckOBBVsCapsule(const OBB& obb, const CapsuleView& capsule,
+		XMFLOAT3* outNormal, float* outPenetration)
+	{
+		return false;
+	}
 }
