4a80a0d23119cd1aa7a25be10ded48b9e8691567
Vallhup <ths1220ths@naver.com>
2026-02-11 02:48:00 +0900

    feat: Implementing WorldScheduler
    Implement WorldScheduler, which is responsible for running Worlds.
    
    Currently, we only provide the level of control that runs according to the TickRate of each world. Future world-specific priorities, tick skip policies, etc. may be added.

---
+++ WITH_Server/WITH_Server_Framework/WorldScheduler.cpp
@@ -0,0 +1,72 @@
+#include "pch.h"
+#include "WorldScheduler.h"
+#include "World.h"
+#include "WorldRegistry.h"
+
+void WorldScheduler::Register(WorldId id, uint32 tickRate)
+{
+	auto it = std::find_if(_entries.begin(), _entries.end(),
+		[&](const Entry& e)
+		{
+			return e.id == id;
+		});
+
+	if (it != _entries.end())
+	{
+		it->tickInterval = TickIntervalFromRate(tickRate);
+		it->acc = 0.0;
+		it->paused = false;
+	}
+
+	else
+	{
+		Entry entry;
+		entry.id = id;
+		entry.tickInterval = TickIntervalFromRate(tickRate);
+		entry.acc = 0.0;
+		entry.paused = false;
+
+		_entries.push_back(entry);
+	}
+}
+
+void WorldScheduler::Unregister(WorldId id)
+{
+	auto it = std::find_if(_entries.begin(), _entries.end(),
+		[&](const Entry& e)
+		{
+			return e.id == id;
+		});
+
+	if (it != _entries.end())
+		_entries.erase(it);
+}
+
+void WorldScheduler::Update(const double dT)
+{
+	const double clampDT = std::clamp<double>(dT, 0.0, 0.25);
+
+	for (Entry& entry : _entries)
+	{
+		if (entry.paused) continue;
+		
+		entry.acc += clampDT;
+
+		if (entry.acc >= entry.tickInterval)
+		{
+			World* world = _reg.GetWorld(entry.id);
+			if (!world)
+			{
+				entry.paused = true;
+				continue;
+			}
+
+			world->Update();
+			entry.acc = 
+				std::max<double>(0.0, entry.acc - entry.tickInterval);
+		}
+	}
+
+	_entries.erase(std::remove_if(_entries.begin(), _entries.end(),
+		[](const Entry& e) { return e.paused; }), _entries.end());
+}
---
+++ WITH_Server/WITH_Server_Framework/WorldScheduler.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "WorldId.h"
+
+class WorldRegistry;
+
+class WorldScheduler {
+public:
+	explicit WorldScheduler(WorldRegistry& reg)
+		: _reg(reg) {}
+
+	void Register(WorldId id, uint32 tickRate);
+	void Unregister(WorldId id);
+
+	void Update(const double dT);
+
+private:
+	struct Entry {
+		WorldId id;
+		double tickInterval{ 0.016 };
+		double acc{ 0.0 };
+		bool paused{ false };
+	};
+
+	WorldRegistry& _reg;
+	std::vector<Entry> _entries;
+};
+
+static inline double TickIntervalFromRate(uint32 tickRate)
+{
+	if (tickRate == 0) tickRate = 1;
+	return 1.0 / double(tickRate);
+}
+
