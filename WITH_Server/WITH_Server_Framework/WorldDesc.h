6c494e445819f2a7113f19f73f561287d365d666
Vallhup <ths1220ths@naver.com>
2026-02-11 20:05:01 +0900

    feat: Defining Parameters required for World configuration
    Implmenting Static Parameters WorldDesc and Dynamic Parameters WorldConfig.

---
+++ WITH_Server/WITH_Server_Framework/WorldDesc.h
@@ -0,0 +1,25 @@
+#pragma once
+
+// WorldDesc
+//  - World 구성에 필요한 불변값들
+//  - 생성자 인자로 초기화 후 변경 불가
+//
+// 1) World Type
+// 2) World Ruleset Id
+//  - 규칙 셋, 스폰 테이블, AI 테이블 등
+// 3) Capacity
+//  - maxPlayers, maxEntities 등
+// 4) Map Id
+// 5) NetPolicy
+// 6) Threading Policy
+
+#include "types.h"
+
+struct WorldDesc {
+	uint8 worldType;
+	int worldRulesetId;
+	uint64 Capacity;
+	int mapId;
+	uint8 netPolicy;
+	uint8 threadingPolicy;
+};
