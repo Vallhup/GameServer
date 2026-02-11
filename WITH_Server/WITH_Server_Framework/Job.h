bc9b8e0a832546248fa7082bb449809e1935b0b0
Vallhup <ths1220ths@naver.com>
2026-02-11 19:55:36 +0900

    chore: Transfer to existing code framework

---
+++ WITH_Server/WITH_Server_Framework/Job.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <functional>
+#include <latch>
+
+#include "Component.h"
+#include "System.h"
+
+struct Job {
+	virtual ~Job() = default;
+};
+
+struct SystemJob : public Job {
+	System* system;
+	const float dT;
+
+	SystemJob(System* s, float deltaTime) : system(s), dT(deltaTime) {}
+	static void Execute(void* ctx)
+	{
+		SystemJob* job = static_cast<SystemJob*>(ctx);
+		job->system->Execute(job->dT);
+	}
+};
+
+extern std::atomic<int> activeJobs;
+extern std::atomic<int> peak;
+
+struct ParallelForJob : public Job {
+	int begin;
+	int end;
+	std::function<void(int, int)> func;
+	std::latch* latch;
+	std::atomic<bool> executed;
+
+	ParallelForJob(int b, int e, const std::function<void(int, int)>& f, std::latch* l)
+		: begin(b), end(e), func(f), latch(l), executed(false) {}
+	static void Execute(void* ctx)
+	{
+		ParallelForJob* job = static_cast<ParallelForJob*>(ctx);
+
+		bool expected{ false };
+		if (job->executed.compare_exchange_strong(expected, true)) {
+			job->func(job->begin, job->end);
+			job->latch->count_down();
+			delete job;
+		}
+	}
+};
