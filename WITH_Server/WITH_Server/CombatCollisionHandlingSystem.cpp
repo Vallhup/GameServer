0652238117e80cc84eb589c50fa7ce98f9211f38
Vallhup <ths1220ths@naver.com>
2026-03-01 02:53:29 +0900

    chore: Code Organization

--- WITH_Server/WITH_Server/CombatCollisionHandlingSystem.cpp
+++ WITH_Server/WITH_Server/CombatCollisionHandlingSystem.cpp
@@ -2,6 +2,7 @@
 #include "CombatCollisionHandlingSystem.h"
 #include "Math.h"
 #include "Tags.h"
+#include "RepComponent.h"
 
 void CombatCollisionHandlingSystem::Execute(const double dT)
 {
@@ -75,16 +76,6 @@ void CombatCollisionHandlingSystem::HandleStrike(const CombatCollisionEvent& eve
 
 	const bool inFront90 = 
 		TransformHelper::IsInFront90_XZ(*aTrans, *vTrans);
-
-	const auto* typeComp = ecs.GetStorage<SpawnTypeComp>().GetComponent(entity);
-	if (!typeComp) continue;
-
-	const auto& pol = ActionManager::Get().GetPolicy(actionState->action, typeComp->type, actionState->attack);
-	const float baseDuration = pol.duration;
-
-	const float atkStartN = 0.683f / baseDuration;
-	const float atkEndN = 0.975f / baseDuration;
-
 
 	const bool parryWindowOn =
 		(actionState->action == ActionType::Parry) &&
