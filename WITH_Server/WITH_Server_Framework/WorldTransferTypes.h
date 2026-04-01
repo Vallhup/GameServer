bca007790ac1fe7bba2234cbac12ade561dd8596
Vallhup <ths1220ths@naver.com>
2026-04-02 01:10:16 +0900

    feat: Implement a data structure for WorldTransfer Entity Snapshot

---
+++ WITH_Server/WITH_Server_Framework/WorldTransferTypes.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
+#include "ComponentStorage.h"
+#include "Entity.h"
+
+using ComponentTypeId = TypeId;
+
+struct TransferComponentSnapshot
+{
+	ComponentTypeId typeId{};
+	std::vector<std::byte> bytes;
+};
+
+struct TransferEntitySnapshot
+{
+	uint32_t connectionId{ 0 };
+	Entity sourceEntity{ Entity::Null() };
+	std::vector<TransferComponentSnapshot> components;
+};
