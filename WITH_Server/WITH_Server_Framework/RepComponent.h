bc94df644745d55e87f31756c0be287d4cd88f7e
Vallhup <ths1220ths@naver.com>
2026-05-05 15:24:31 +0900

    chore: Code organization

--- WITH_Server/WITH_Server_Framework/RepComponent.h
+++ WITH_Server/WITH_Server_Framework/RepComponent.h
@@ -23,7 +23,7 @@ struct SpawnTypeComp : Component
 struct AITypeComp : Component
 {
 	AIArchetype aiType;
-	uint16_t aiTuningId{ 0 };
+	uint64_t aiProfileId{ 0 };
 };
 
 struct ReplicatedTag : TagComponent { };
