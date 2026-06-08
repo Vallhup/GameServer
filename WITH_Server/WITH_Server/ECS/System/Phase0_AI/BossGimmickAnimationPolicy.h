0570ea626f2e6dff274a58c044ea2047e2a2c921
Vallhup <ths1220ths@naver.com>
2026-06-09 01:10:37 +0900

    chore: Adjust FinalBoss Gimmick Peach Time

--- WITH_Server/WITH_Server/ECS/System/Phase0_AI/BossGimmickAnimationPolicy.h
+++ WITH_Server/WITH_Server/ECS/System/Phase0_AI/BossGimmickAnimationPolicy.h
@@ -4,7 +4,7 @@
 
 namespace BossGimmickAnimationPolicy
 {
-	inline constexpr float kEntryAnimationDurationSec = 10.0f;
+	inline constexpr float kEntryAnimationDurationSec = 11.5f;
 
 	inline AnimationId EntryAnimationFor(BossGimmickType type) noexcept
 	{
